39bb9a810aabb15297b4feb063bb033f08eaac71
Joseph Rivera <josephdrivera15@gmail.com>
2024-06-14 12:21:42 -0400

    Add redirections and improve form validation display
    This update includes adding a redirection to tasks index on the root route for immediate access to tasks, and enhancing the form validation feedback. Errors on task addition are now better displayed with distinct styles and messages.

--- routes/web.php
+++ routes/web.php
@@ -15,31 +15,38 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('/', function () {
+    return redirect()->route('tasks.index');
+});
+
 Route::get('/tasks', function () {
     return view('index', [
         'tasks' => Task::latest()->get()
-    ]); // get all tasks and pass them to the
-})->name('tasks.index'); // name the route tasks.index
+    ]);
+})->name('tasks.index');
+
+Route::view('/tasks/create', 'create')
+    ->name('tasks.create');
 
 Route::get('/tasks/{id}', function ($id) {
     return view('show', [
         'task' => Task::findOrFail($id)
-    ]); // find the task by id
-})->name('tasks.show'); // name the route tasks.show
-
+    ]);
+})->name('tasks.show');
 
 Route::post('/tasks', function (Request $request) {
     $data = $request->validate([
         'title' => 'required|max:255',
         'description' => 'required',
         'long_description' => 'required'
-    ]); // validate the request
+    ]);
 
-    $task = new Task; // new instance of Task model
-    $task->title = $data['title']; // set the title attribute
+    $task = new Task;
+    $task->title = $data['title'];
     $task->description = $data['description'];
     $task->long_description = $data['long_description'];
-    $task->save(); // save the task to the database
+    $task->save();
 
-    return redirect()->route('tasks.show', ['id' => $task->id]);
-})->name('tasks.store'); // name the route tasks.store
+    return redirect()->route('tasks.show', ['id' => $task->id])
+        ->with('success', 'Task created successfully!');
+})->name('tasks.store');
