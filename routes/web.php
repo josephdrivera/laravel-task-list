42c2d35aca78b619e77ddc78b10f8521de38f236
Joseph Rivera <josephdrivera15@gmail.com>
2024-06-14 11:56:58 -0400

    Refactor routes and add task creation functionality
    The Task model data handling has been refactored in the routes/web.php file to use Eloquent ORM methods instead of array manipulation. This refactoring simplifies the data retrieval and manipulation process. Additionally, a new route and corresponding logic and validation rules have been added to support task creation. An associated view is also added, allowing users to enter new tasks.

--- routes/web.php
+++ routes/web.php
@@ -1,74 +1,45 @@
 <?php
 
-class Task
-{
-    public function __construct(
-        public int $id,
-        public string $title,
-        public string $description,
-        public ?string $long_description,
-        public bool $completed,
-        public string $created_at,
-        public string $updated_at
-    ) {
-    }
-}
+use App\Models\Task;
+use Illuminate\Http\Response;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
-$tasks = [
-    new Task(
-        1,
-        'Buy groceries',
-        'Task 1 description',
-        'Task 1 long description',
-        false,
-        '2023-03-01 12:00:00',
-        '2023-03-01 12:00:00'
-    ),
-    new Task(
-        2,
-        'Sell old stuff',
-        'Task 2 description',
-        null,
-        false,
-        '2023-03-02 12:00:00',
-        '2023-03-02 12:00:00'
-    ),
-    new Task(
-        3,
-        'Learn programming',
-        'Task 3 description',
-        'Task 3 long description',
-        true,
-        '2023-03-03 12:00:00',
-        '2023-03-03 12:00:00'
-    ),
-    new Task(
-        4,
-        'Take dogs for a walk',
-        'Task 4 description',
-        null,
-        false,
-        '2023-03-04 12:00:00',
-        '2023-03-04 12:00:00'
-    ),
-];
-
-Route::get('/', function () {
-    return redirect()->route('tasks.index');
-});
-
-Route::get('/tasks', function () use ($tasks) {
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+Route::get('/tasks', function () {
     return view('index', [
-        'tasks' => $tasks
-    ]);
-})->name('tasks.index');
+        'tasks' => Task::latest()->get()
+    ]); // get all tasks and pass them to the
+})->name('tasks.index'); // name the route tasks.index
 
-Route::get('/tasks/{id}', function ($id) use ($tasks) {
-    $task = collect($tasks)->firstWhere('id', $id);
+Route::get('/tasks/{id}', function ($id) {
+    return view('show', [
+        'task' => Task::findOrFail($id)
+    ]); // find the task by id
+})->name('tasks.show'); // name the route tasks.show
 
-    if (!$task) {
-        abort(Response::HTTP_NOT_FOUND);
-    }
 
-    return view('show', ['task' => $task]);
-})->name('tasks.show');
+Route::post('/tasks', function (Request $request) {
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'description' => 'required',
+        'long_description' => 'required'
+    ]); // validate the request
+
+    $task = new Task; // new instance of Task model
+    $task->title = $data['title']; // set the title attribute
+    $task->description = $data['description'];
+    $task->long_description = $data['long_description'];
+    $task->save(); // save the task to the database
+
+    return redirect()->route('tasks.show', ['id' => $task->id]);
+})->name('tasks.store'); // name the route tasks.store
